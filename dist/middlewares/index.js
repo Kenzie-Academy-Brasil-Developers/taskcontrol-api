f9ea57f28ea1e10e3adffbf590442518502e730f
Matheus Batista <mabatista_10@hotmail.com>
2024-03-01 19:57:58 -0300

    TaskControl API

--- dist/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ensure = exports.handleErrors = exports.auth = void 0;
-const auth_middleware_1 = require("./auth.middleware");
-Object.defineProperty(exports, "auth", { enumerable: true, get: function () { return auth_middleware_1.auth; } });
-const handleErrors_middleware_1 = require("./handleErrors.middleware");
-Object.defineProperty(exports, "handleErrors", { enumerable: true, get: function () { return handleErrors_middleware_1.handleErrors; } });
-const ensure_middleware_1 = require("./ensure.middleware");
-Object.defineProperty(exports, "ensure", { enumerable: true, get: function () { return ensure_middleware_1.ensure; } });
